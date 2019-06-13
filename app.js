03156f21390823a3ea68d498ac69c2bbc8a7afff
AJ Kwok <pc7789@AJs-Mac-mini.local>
2019-06-13 03:22:47 -0400

    test commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 9090;
 
 app.use(express.static(path.join(__dirname, '/')));
 app.get('/', (req, res) => res.sendFile(__dirname + 'index.html'));
